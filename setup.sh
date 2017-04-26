1b2686f90e55c29bb7a8dc271ec2db48670b3f7a
sert <demo.sert@gmail.com>
2017-04-26 17:38:30 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
-wget http://dl.google.com/android/android-sdk_r24.0.2-linux.tgz
-tar zxvf android-sdk_r24.0.2-linux.tgz
-sudo mv android-sdk-linux/ /opt/
-rm android-sdk_r24.0.2-linux.tgz
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u73-b02/jdk-8u73-linux-x64.rpm"
+sudo yum -y install jdk-8u73-linux-x64.rpm
+echo 'export JAVA_HOME=/usr/java/default' >> ~/.zshrc
 
-echo 'export ANDROID_HOME=/opt/android-sdk-linux' >> ~/.zshrc
+wget https://dl.google.com/android/repository/tools_r25.2.3-linux.zip
+unzip tools_r25.2.3-linux.zip
+sudo mv tools/ /opt/android-sdk
+rm tools_r25.2.3-linux.zip
+
+echo 'export ANDROID_HOME=/opt/android-sdk' >> ~/.zshrc
 echo 'export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools' >> ~/.zshrc
 
 echo 'copy your local $ANDROID_HOME/licenses files to the server'
