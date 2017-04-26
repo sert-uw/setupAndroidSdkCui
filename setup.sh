ecdb1758738c394b2a777ec71475c492bade9922
sert <demo.sert@gmail.com>
2017-04-26 16:11:05 +0900

    change update sdk

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ rm android-sdk_r24.0.2-linux.tgz
 echo 'export ANDROID_HOME=/opt/android-sdk-linux' >> ~/.zshrc
 echo 'export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools' >> ~/.zshrc
 
-android update sdk --no-ui
 android list sdk --all
+android update sdk --no-ui --filter 1,2,5,36,37,38,39,42,165,166,167,169,171,172,173,174,175
